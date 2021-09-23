a45dc8792ba587affa5037baa4b59885c0e69983
joshua-8 <joshua_phelps@brown.edu>
2021-09-23 10:37:54 -0400

    some more empty functions and Interrupt stuff

--- src/SplitFlap.h
+++ src/SplitFlap.h
@@ -1,12 +1,18 @@
 #ifndef _SPLIT_FLAP_H_
 #define _SPLIT_FLAP_H_
+
+#define SF_MAKE_ISR_MACRO(name) \
+    void name##_SF_ISR() { name.interruptCallback(); } //helps with setting up interrupts
+
 #include <AccelStepper.h>
 class SplitFlap {
 public:
     AccelStepper* motor;
+    byte zeroSensorPin;
     SplitFlap(AccelStepper* _motor, byte _zeroSensorPin)
         : motor(_motor)
     {
+        zeroSensorPin = _zeroSensorPin;
     }
     /**
      * @brief  call this in setup()
@@ -40,6 +46,23 @@ public:
     void run()
     {
         motor->run();
+    }
+    /**
+     * @brief  ISRs can't be inside classes, so make an ISR that calls this
+     * @retval None
+     */
+    void interruptCallback()
+    {
+    }
+
+    /**
+     * @note interrupt created by SF_MAKE_ISR_MACRO(name of SplitFlap) will be of the form <name of SplitFlap instance>_SF_ISR
+     */
+    void setUpInterrupts(void (*_isrPointer)(void))
+    {
+        pinMode(zeroSensorPin, INPUT);
+
+        attachInterrupt(digitalPinToInterrupt(zeroSensorPin), _isrPointer, CHANGE);
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 SplitFlap module1 = SplitFlap( //a split flap needs a pointer to an accelStepper and pins and info
     &AccelStepper(AccelStepper::HALF4WIRE, 8, 9, 10, 11), //https://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html#a3bc75bd6571b98a6177838ca81ac39ab
     2); //zero sensor pin,
+SF_MAKE_ISR_MACRO(module1); //create interrupt service routine
 void setup()
 {
+    Serial.begin(9600);
     module1.begin();
+    module1.setUpInterrupts(module1_SF_ISR);
 }
 
 void loop()
@@ -16,5 +19,16 @@ void loop()
 
 char letterToFlapNumber(char letter)
 {
-    return letter - 64;
+    return letter - 64; //A=1, B=2 ...
+}
+
+char returnCharOnTimer()
+{
+    //use for testing, return 0 if nothing to display yet
+    return 0;
+}
+
+char returnCharSerialComm()
+{
+    return 0;
 }
